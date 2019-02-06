2c1ffa361094425be7213f2b0e8cfc1959ae1e54
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-06 20:50:03 +0000

    changed travis.yml to reset, also changed the schema to remove title as migrations does this for us

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_02_06_190425) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
